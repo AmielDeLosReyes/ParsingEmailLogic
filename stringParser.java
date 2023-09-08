c5c3b90c9d438c91dc8c1f429a89a904c947400e
Amiel De Los Reyes <89047257+AmielDeLosReyes@users.noreply.github.com>
2023-09-08 13:26:02 +0800

    Update stringParser.java

--- stringParser.java
+++ stringParser.java
@@ -8,8 +8,10 @@ public class Main {
         // hardcoded string
         String str2 = "First Name: Amiel\nLast Name: De Los Reyes\nLDAP: ADELO71\nEmail Address: adelo71@safeway.com\nRequest Type: Add\nAcceptance: Yes\nDevelopment: Yes\nOMS Admin (All Roles): Yes\nOMS MF Builder: Yes";
 
+        String str3 = "First Name: Amiel\nLast Name: De Los Reyes\nLDAP: ADELO71\nEmail Address: adelo71@safeway.com\nRequest Type: Add\nAcceptance: Yes\nDevelopment: Yes\nProduction: Yes\nOMS Admin (All Roles): Yes\nOMS MF Builder: Yes\nTesting: Yes\nAno pa: Yes";
+
         // split the string into semicolons and new lines
-        String[] s = str2.split("[:\n]");
+        String[] s = str3.split("[:\n]");
 
         // put the key-value pair result into a linked hash map
         LinkedHashMap<String, String> result = new LinkedHashMap<String, String>();
@@ -21,6 +23,24 @@ public class Main {
         // odd index are key, even index are value
         for(int i = 0; i < s.length; i++) {
             if(i % 2 == 0) {
+                switch(s[i]) {
+                    case "First Name":
+                        s[i] = "first_name";
+                        break;
+
+                    case "Last Name":
+                        s[i] = "last_name";
+                        break;
+
+                    case "LDAP":
+                        s[i] = "user_id";
+                        break;
+
+                    case "Email Address":
+                        s[i] = "email";
+                        break;
+                }
+
                 fields.add(s[i]);
             }else {
                 values.add(s[i].trim());
